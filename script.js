48feccec8349e09f49e2b989e0897b0e99b94c1f
Marcin Wosinek <marcin.wosinek@gmail.com>
2013-03-22 15:03:41 +0100

    Hello world

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-/* Script */
+// Create a new module
+var workshop = angular.module('workshop', []);
+
+// Create routes
+workshop.config(function($routeProvider) {
+  $routeProvider.when('/index', {
+      templateUrl: 'views/index.html',
+      controller: 'IndexCtrl'
+    })
+    .otherwise({
+    redirectTo: '/index'
+  });
+});
+
+// Create controllers
+workshop.controller('IndexCtrl', function($scope) {
+  $scope.helloMessage = "Hello world";
+});
