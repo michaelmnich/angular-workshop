a78948b14d6acfbd1aea85bf28a1b63b0d09aa37
Marcin Wosinek <marcin.wosinek@gmail.com>
2013-03-22 17:10:58 +0100

    Basic filter usage

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ workshop.config(function($routeProvider) {
       templateUrl: 'views/index.html',
       controller: 'IndexCtrl'
     })
+    .when('/list', {
+      templateUrl: 'views/list.html',
+      controller: 'ListCtrl'
+    })
     .otherwise({
     redirectTo: '/index'
   });
@@ -15,4 +19,24 @@ workshop.config(function($routeProvider) {
 // Create controllers
 workshop.controller('IndexCtrl', function($scope) {
   $scope.helloMessage = "Hello world";
+});
+workshop.controller('ListCtrl', function($scope) {
+  $scope.list = [
+    {
+      name: "John",
+      surname: "Smith",
+    },
+    {
+      name: "Mark",
+      surname: "Lorem",
+    },
+    {
+      name: "Sally",
+      surname: "Ipsum",
+    },
+    {
+      name: "Frank",
+      surname: "Sky",
+    }
+  ];
 });
